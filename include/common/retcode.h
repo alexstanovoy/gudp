b15b3bd8643aa37ab720fd6505ffb1d97fd4fc37
Alexander Stanovoy <alex.stanovoy@gmail.com>
2020-05-04 14:42:23 +0300

    Predictable error numbers

--- include/common/retcode.h
+++ include/common/retcode.h
@@ -15,31 +15,31 @@ typedef enum {
   /// Successiful return.
   SUCCESS = 0,
   /// General error; malloc() failed.
-  NOT_ENOUGH_MEMORY,
+  NOT_ENOUGH_MEMORY = 1,
   /// SocketsStartup() error; WSAStartup() != NO_ERROR.
-  SOCKET_STARTUP,
+  SOCKET_STARTUP = 2,
   /// SocketsShutdown() error; WSACleanup() != NO_ERROR.
-  SOCKET_SHUTDOWN,
+  SOCKET_SHUTDOWN = 3,
   /// SocketInit() error; Socket cretion failed.
-  SOCKET_INIT,
+  SOCKET_INIT = 4,
   /// SocketBind() error; Socket bind failed.
-  SOCKET_BIND,
+  SOCKET_BIND = 5,
   /// SocketConnect() error; Socket connect failed.
-  SOCKET_CONNECT,
+  SOCKET_CONNECT = 6,
   /// SocketSend() error; Sending failed.
-  SOCKET_SEND,
+  SOCKET_SEND = 7,
   /// SocketReceive() error; Receive failed.
-  SOCKET_RECEIVE,
-  SOCKET_MAKE_NONBLOCKING,
+  SOCKET_RECEIVE = 8,
+  SOCKET_MAKE_NONBLOCKING = 9,
   /// SocketReceive() error; Returned after timeout reached. See
   /// SocketSetTimeout() for details.
-  SOCKET_TIMEOUT,
+  SOCKET_TIMEOUT = 10,
   /// SocketSetTimeout() error; Setting failed.
-  SOCKET_SETTIMEOUT,
+  SOCKET_SETTIMEOUT = 11,
   /// Server cannot find specified user id.
-  SERVER_USER_NOT_FOUND,
+  SERVER_USER_NOT_FOUND = 12,
   /// Server tried to accept connection but there were no place in server.
-  SERVER_CROWDED,
+  SERVER_CROWDED = 13,
   /// Client received disconnect packet while in action.
-  CLIENT_KICKED,
+  CLIENT_KICKED = 14,
 } RETCODE;
